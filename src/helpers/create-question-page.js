0e990e2a6e516f1182431450c1ec9b3cb8b2c46e
Connor Cartwright <connorcart@gmail.com>
2016-08-13 15:24:01 +0100

    Started working on question area creation

--- src/helpers/create-question-page.js
+++ src/helpers/create-question-page.js
@@ -1,18 +1,34 @@
 var fs = require('fs');
 var baseURL = './questions';
 
-// Function createTypeArea(questionType, answers) {
-//
-// }
+function createMultipleChoiceOption(optionText) {
+
+}
+
+function createTypeArea(questionType, answers) {
+   'use strict';
+
+   var r = '';
+
+   if (questionType === 'Multiple Choice') {
+      answers = JSON.parse(answers);
+
+      for(var i = 0; i < answers.length; i++) {
+         r += '<div class="multiple-choice-option"></div><span>' + answers[i].optionText + '</span>';
+      }
+   }
+
+   return r;
+}
 
 function buildQuestionPage(data) {
    'use strict';
 
    var body = '<body><h1>' + data.questionType + ' Question</h1><h2>' + data.questionName +
-     '</h2><p>' + data.questionStatement + '</p></body>';
+     '</h2><p>' + data.questionStatement + '</p>' + createTypeArea(data.questionType, data.answers) + '</body>';
 
    var html = '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>' + data.questionName + '</title>' +
-     '<link rel="stylesheet" href="css/style.css"><script src="script.js"></script></head>' +
+     '<link rel="stylesheet" href="../css/style.css"><script src="script.js"></script></head>' +
      body + '</html>';
 
    return html;
