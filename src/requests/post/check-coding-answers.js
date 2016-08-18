14f77c8af8b324466f3e887305f08f242a7b0ff7
Connor Cartwright <connorcart@gmail.com>
2016-08-18 18:46:52 +0100

    Removed console logs

--- src/requests/post/check-coding-answers.js
+++ src/requests/post/check-coding-answers.js
@@ -3,18 +3,11 @@ function checkCodingAnswers(database, reqData, callback) {
    'use strict';
 
    database.answerQueries.getCodingOutputs(reqData.questionID, function(data) {
-      console.log(data);
       var output = JSON.parse(reqData.output);
-
-      console.log(output);
-
       var result = [];
       var passed = true;
 
       for(var i = 0; i < data.length; i++) {
-         console.log('Database ' + i + ' : ' + data[i].output);
-         console.log('Reqdata ' + i + ' : ' + output[i]);
-
          if (data[i].output !== output[i]) {
             result.push(false);
             passed = false;
