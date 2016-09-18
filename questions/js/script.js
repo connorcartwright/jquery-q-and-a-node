9a741ab723a6054a9af97b3dc45a2df166aa2452
Connor Cartwright <connorcart@gmail.com>
2016-09-18 20:23:15 +0100

    Update question main to grunt standards

--- questions/js/script.js
+++ questions/js/script.js
@@ -1,42 +1,43 @@
 
 $('.js-hint').on('click', function() {
-  if (!$(this).hasClass('js-hint-active')) {
-    var $activeHint =  $('.js-hint-active');
-    var activeHintNum = $activeHint.data('hint');
-    var newHintNum = $(this).data('hint');
+   'use strict';
 
-    $activeHint
-      .removeClass('js-hint-active')
-      .find('span')
-      .text(activeHintNum);
-    $(this)
-      .addClass('js-hint-active')
-      .find('span')
-      .text('Hint ' + newHintNum);
-    $('.qa-hint-text').text($(this).data('hint-text'));
-  }
+   if (!$(this).hasClass('js-hint-active')) {
+      var $activeHint = $('.js-hint-active');
+      var activeHintNum = $activeHint.data('hint');
+      var newHintNum = $(this).data('hint');
 
+      $activeHint
+        .removeClass('js-hint-active')
+        .find('span')
+        .text(activeHintNum);
+      $(this)
+        .addClass('js-hint-active')
+        .find('span')
+        .text('Hint ' + newHintNum);
+      $('.qa-hint-text').text($(this).data('hint-text'));
+   }
 });
 
 $('.js-hint-trigger').on('click', function() {
-  $('.qa-hints-area').toggleClass('closed');
+   'use strict';
+
+   $('.qa-hints-area').toggleClass('closed');
 });
 
 function displayMessage(message, success) {
-  var $message = $('<div class="qa-message-overlay"></div>');
-  $message.append('<div class="qa-message ' + (success ? 'correct' : 'wrong') + '"><span>' + message + '</span></div>');
-  $message.hide();
-  $('.qa-question-content').append($message);
+   'use strict';
 
-  $message
-    .fadeIn(800)
-    .delay(1000)
-    .fadeOut(1500, function() {
+   var $message = $('<div class="qa-message-overlay"></div>');
+
+   $message.append('<div class="qa-message ' + (success ? 'correct' : 'wrong') + '"><span>' + message + '</span></div>');
+   $message.hide();
+   $('.qa-question-content').append($message);
+
+   $message
+     .fadeIn(800)
+     .delay(1000)
+     .fadeOut(1500, function() {
       $(this).remove();
-    });
+   });
 }
-
-
-
-
-
