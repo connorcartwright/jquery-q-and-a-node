e70c2da5b2cad72b683e55b4958d140679ca8c02
Connor Cartwright <connorcart@gmail.com>
2016-09-18 20:25:15 +0100

    Add whitespace

--- src/requests/post/add-page.js
+++ src/requests/post/add-page.js
@@ -2,6 +2,7 @@ function addPage(database, reqData, callback) {
    'use strict';
 
    database.pageQueries.addPage(reqData.id, reqData.title);
+
    var response = {
       status: 200,
       success: 'Added Successfully'
