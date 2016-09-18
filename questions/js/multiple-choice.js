cd6680b3894b241b1177ea8c8d8562cb76db1be6
Connor Cartwright <connorcart@gmail.com>
2016-09-18 20:23:00 +0100

    Update multiple choice to grunt standards

--- questions/js/multiple-choice.js
+++ questions/js/multiple-choice.js
@@ -1,64 +1,73 @@
+require('bind-hints');
+var displayMessage = require('./display-message');
+
 $('.js-mc-option').on('click', function() {
-  $(this)
-    .toggleClass('correct')
-    .prev()
-    .toggleClass('correct');
+   'use strict';
+
+   $(this)
+     .toggleClass('correct')
+     .prev()
+     .toggleClass('correct');
 });
 
 function getMultipleChoiceData(pageID, questionID) {
-  var options = [];
+   'use strict';
 
+   var options = [];
 
-  $('.multiple-choice-option').each(function() {
-    options.push($(this).find('.correct').length ? 1 : 0);
-  });
+   $('.multiple-choice-option').each(function() {
+      options.push($(this).find('.correct').length ? 1 : 0);
+   });
 
-  var data = {
-    pageID: pageID,
-    questionID: questionID,
-    action: 'checkMultipleChoiceAnswers',
-    options: JSON.stringify(options)
-  };
+   var data = {
+      pageID: pageID,
+      questionID: questionID,
+      action: 'checkMultipleChoiceAnswers',
+      options: JSON.stringify(options)
+   };
 
-  return data;
+   return data;
 }
 
 $('.js-submit-question').on('click', function() {
-  var pageID = $('.qa-question').data('pageid');
-  var questionID = $('.qa-question').data('questionid');
+   'use strict';
 
-  var postData = getMultipleChoiceData(pageID, questionID);
+   var pageID = $('.qa-question').data('pageid');
+   var questionID = $('.qa-question').data('questionid');
 
-  $.ajax({
-    url: 'http://SERVER_IP:PORT',
-    method: 'POST',
-    data: postData,
-    dataType: 'json',
-    crossDomain: true
-  })
-    .done(function() {
+   var postData = getMultipleChoiceData(pageID, questionID);
+
+   $.ajax({
+      url: 'http://localhost:8080',
+      method: 'POST',
+      data: postData,
+      dataType: 'json',
+      crossDomain: true
+   })
+     .done(function() {
       console.log('done/success');
-    })
-    .fail(function() {
+   })
+     .fail(function() {
       console.log('fail/error');
-    })
-    .always(function(data) {
+   })
+     .always(function(data) {
       console.log('always');
       console.log(data);
+
       if (data.success) {
-        displayMessage('Correct!', true);
+         displayMessage('Correct!', true);
       } else {
-        var message = '';
-        if (data.numCorrect === 0) {
-          var numSelected = $('.multiple-choice .mc-option-text.correct').length;
-          message = '<span class="small">All of your selected options are incorrect!</span>';
-        } else {
-          message = '<span class="small">' + data.numCorrect + ' of your selected options are correct!</span>';
-        }
-        displayMessage('Incorrect! <br> ' + message, false);
+         var message = '';
+
+         if (data.numCorrect === 0) {
+            // Var numSelected = $('.multiple-choice .mc-option-text.correct').length;
+            message = '<span class="small">All of your selected options are incorrect!</span>';
+         } else {
+            message = '<span class="small">' + data.numCorrect + ' of your selected options are correct!</span>';
+         }
+
+         displayMessage('Incorrect! <br> ' + message, false);
       }
-    });
-
+   });
 });
-
 
